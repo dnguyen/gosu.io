9c67e679532810259778921f6976ef14d015ef81
Dylan Nguyen <d_nguyen@live.com>
2013-10-15 17:50:47 -0400

    Make sure data has been loaded before rendering main page content.

--- assets/js/app/controllers/MainController.js
+++ assets/js/app/controllers/MainController.js
@@ -14,68 +14,65 @@ define([
 
     var GosuApp = namespace.app;
 
+    function LoadMetaCollection(collection, url, count) {
+        collection.url = url;
+        return collection.fetch({
+            data : { count : count }
+        });
+    }
+
     return {
         // TODO: put rendering in views
+        //       move sidebar logic to its own controller
         mainPage : function() {
 
-            /*
-                Get popular
-                Temp use most viewed.
-                TODO: Create popular algorithm
-             */
             var popularTracksCollection = new Backbone.Collection();
-            popularTracksCollection.url = "http://localhost/gosukpop-api/public/MostViewedTracks";
-            popularTracksCollection.fetch({
-                data : { count : 8 }
-            });
-
-            var popularTracksCompositeView = new TrackGroupCollectionView({
-                collection : popularTracksCollection
-            });
-
-            /*
-                Get new releases
-             */
             var newReleasesCollection = new Backbone.Collection();
-            newReleasesCollection.url = "http://localhost/gosukpop-api/public/NewTrackReleases";
-            newReleasesCollection.fetch({
-                data : { count : 8 }
-            });
-
-            var newReleasesCollectionView = new TrackGroupCollectionView({
-                collection : newReleasesCollection
-            });
-
             var comingSoonCollection = new Backbone.Collection();
-            comingSoonCollection.url = "http://localhost/gosukpop-api/public/ComingSoonTracks";
-            comingSoonCollection.fetch({
-                data : { count : 5 }
-            });
-            console.log(comingSoonCollection);
-            /*
-                Render content layout
-             */
-            GosuApp.content.show(GosuApp.contentLayout);
-            GosuApp.contentLayout.popular.show(popularTracksCompositeView);
-            GosuApp.contentLayout.newReleases.show(newReleasesCollectionView);
 
             /*
-                Render side bar modules
+                Only start rendering page once all of the data is ready.
              */
-            var sideModulesLayout = new SideModulesLayout();
-            var sotwView = new SOTWSideModuleView();
-            var radioView = new RadioSideModuleView();
-            var moduleListView = new ModuleListCompositeView({
-                collection : comingSoonCollection,
-                itemViewOptions: {
-                    moduleType : "coming-soon"
-                }
+            $.when(
+                LoadMetaCollection(popularTracksCollection, "http://localhost/gosukpop-api/public/MostViewedTracks", 8),
+                LoadMetaCollection(newReleasesCollection, "http://localhost/gosukpop-api/public/NewTrackReleases", 8),
+                LoadMetaCollection(comingSoonCollection, "http://localhost/gosukpop-api/public/ComingSoonTracks", 5)
+            ).then(function() {
+
+                // Render popular tracks
+                var popularTracksCompositeView = new TrackGroupCollectionView({
+                    collection : popularTracksCollection
+                });
+
+                // Render new releases
+                var newReleasesCollectionView = new TrackGroupCollectionView({
+                    collection : newReleasesCollection
+                });
+
+                // Start rendering content region
+                GosuApp.content.show(GosuApp.contentLayout);
+                GosuApp.contentLayout.popular.show(popularTracksCompositeView);
+                GosuApp.contentLayout.newReleases.show(newReleasesCollectionView);
+
+                // Render side modules
+                var sideModulesLayout = new SideModulesLayout();
+                var sotwView = new SOTWSideModuleView();
+                var radioView = new RadioSideModuleView();
+                var moduleListView = new ModuleListCompositeView({
+                    collection : comingSoonCollection,
+                    // Pass module type so we can change the template based on the side module
+                    // Uses 1 side module view instead of a seperate view file for each side module
+                    itemViewOptions: {
+                        moduleType : "coming-soon"
+                    }
+                });
+
+                GosuApp.contentLayout.sideModules.show(sideModulesLayout);
+                sideModulesLayout.sotw.show(sotwView);
+                sideModulesLayout.radio.show(radioView);
+                sideModulesLayout.comingSoon.show(moduleListView);
             });
 
-            GosuApp.contentLayout.sideModules.show(sideModulesLayout);
-            sideModulesLayout.sotw.show(sotwView);
-            sideModulesLayout.radio.show(radioView);
-            sideModulesLayout.comingSoon.show(moduleListView);
         },
 
         tracksPage : function() {
