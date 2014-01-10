fef553b5c47ced871a54974eadf86e3c833bb755
Dylan Nguyen <d_nguyen@live.com>
2014-01-10 01:55:24 -0500

    Add overlay for track thumbnails

--- assets/js/app/views/TrackGroupItemView.js
+++ assets/js/app/views/TrackGroupItemView.js
@@ -10,7 +10,25 @@ define([
         tagName : 'li',
         className : 'item',
 
-        template: _.template(TrackGroupItemTemplate)
+        template: _.template(TrackGroupItemTemplate),
+
+        events : {
+            "mouseover .thumb" : "thumbnailHover",
+            "mouseleave .thumb" : "thumbnailLeaveHover"
+        },
+
+        // Display overlay navigation over thumbnail when hovering over the thumbnail
+        thumbnailHover : function(e) {
+            // If thumbnail doesn't already have an overlay, append an overlay.
+            if ($(e.currentTarget).find(".overlay").length <= 0) {
+                $(e.currentTarget).append('<div class="overlay"><ul class="nav"><li><a href="#">L</a></li><li><a href="#">+</a></li></ul></div>');
+            }
+        },
+
+        // Remove any overlay when not hovering over the thumbnail
+        thumbnailLeaveHover : function(e) {
+            $(e.currentTarget).find(".overlay").remove();
+        }
 
     });
 
